74cc1c274723ca277a21122ec3e2ca8379f5fd7f
Mark Cornick <mark@markcornick.com>
2015-03-25 15:55:20 -0400

    update to ag version 0.29.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default.the_silver_searcher.version = '0.21.0'
-default.the_silver_searcher.checksum = 'ee921373e2bb1a25c913b0098ab946d137749b166d340a8ae6d88a554940a793'
+default.the_silver_searcher.version = '0.29.1'
+default.the_silver_searcher.checksum = '779eb781751fd3dc07bff3b88f4475ed78eea3fa7b21139d8230d17c31db072a'
 default.the_silver_searcher.url = "https://codeload.github.com/ggreer/the_silver_searcher/tar.gz/#{node.the_silver_searcher.version}"
-default.the_silver_searcher.build_opt = ""
+default.the_silver_searcher.build_opt = ''
