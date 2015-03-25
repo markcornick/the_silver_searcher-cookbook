f4d52a297116524453863f2e73e48dc5f48ef238
Mark Cornick <mark@markcornick.com>
2015-03-25 15:58:52 -0400

    update to 1.4.0
    also, only claim to support platforms that are tested

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,12 @@
-name             'the_silver_searcher'
-maintainer       'Mark Cornick'
-maintainer_email 'mark@teamsnap.com'
-license          'MIT'
-description      'Installs/Configures The Silver Searcher'
-depends          'build-essential'
-depends          'chef-sugar'
-version          '1.3.9'
+name 'the_silver_searcher'
+maintainer 'Mark Cornick'
+maintainer_email 'mark@cornick.io'
+license 'MIT'
+description 'Installs/Configures The Silver Searcher'
+depends 'build-essential'
+depends 'chef-sugar'
+version '1.4.0'
 
-%w(debian ubuntu linuxmint raspbian redhat centos oracle scientific amazon
-   enterpriseenterprise fedora).each do |platform|
+%w(debian ubuntu redhat centos fedora).each do |platform|
   supports platform
 end
