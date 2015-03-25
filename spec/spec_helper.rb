9c134fdefcc13442a783e63aa6117a4903301602
Mark Cornick <mark@markcornick.com>
2015-03-25 15:51:03 -0400

    modernize chefspecs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,5 +5,3 @@ RSpec.configure do |config|
   config.platform = 'ubuntu'
   config.version  = '12.04'
 end
-
-at_exit { ChefSpec::Coverage.report! }
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,7 +1,10 @@
-require_relative '../spec_helper'
+require 'spec_helper'
 
 describe 'the_silver_searcher::default' do
-  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+  let(:chef_run) do
+    runner = ChefSpec::ServerRunner.new
+    runner.converge(described_recipe)
+  end
 
   it 'installs chef-sugar' do
     expect(chef_run).to install_chef_gem('chef-sugar')
@@ -9,7 +12,6 @@ describe 'the_silver_searcher::default' do
 
   %w(automake pkg-config zlib1g-dev libpcre3-dev liblzma-dev
      bison autoconf binutils-doc flex build-essential).each do |p|
-
     it "installs #{p}" do
       expect(chef_run).to install_package(p)
     end
@@ -17,7 +19,7 @@ describe 'the_silver_searcher::default' do
 
   it 'downloads and compiles the_silver_searcher' do
     version = chef_run.node.the_silver_searcher.version
-    cache_file = "#{Chef::Config["file_cache_path"]}/the_silver_searcher-#{version}.tar.gz"
+    cache_file = "#{Chef::Config['file_cache_path']}/the_silver_searcher-#{version}.tar.gz"
     expect(chef_run).to create_remote_file(cache_file)
     expect(chef_run.remote_file(cache_file)).to notify('bash[install ag]').to(:run)
     # NOTE: the default action on bash[install ag] is :nothing;
