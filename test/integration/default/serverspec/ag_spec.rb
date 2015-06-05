9994204e71208e30e42252dc523689ee04e779dc
Mark Cornick <mark@markcornick.com>
2015-06-05 19:40:45 -0400

    explicit path to the ag we're testing

--- test/integration/default/serverspec/ag_spec.rb
+++ test/integration/default/serverspec/ag_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 usage = /Usage: ag \[FILE-TYPE\] \[OPTIONS\] PATTERN \[PATH\]/
 
-describe command('ag --help') do
+describe command('/usr/local/bin/ag --help') do
   describe 'its standard output' do
     subject { super().stdout }
     it { is_expected.to match(usage) }
