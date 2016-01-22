b7ad451beb528dad4f745dbc1913e75d6e9f404e
Mark Cornick <mark@markcornick.com>
2016-01-21 19:51:45 -0500

    the_silver_searcher version 0.31.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default.the_silver_searcher.version = '0.30.0'
-default.the_silver_searcher.checksum = 'a3b61b80f96647dbe89c7e89a8fa7612545db6fa4a313c0ef8a574d01e7da5db'
+default.the_silver_searcher.version = '0.31.0'
+default.the_silver_searcher.checksum = '61bc827f4557d8108e91cdfc9ba31632e2568b26884c92426417f58135b37da8'
 default.the_silver_searcher.url = "https://codeload.github.com/ggreer/the_silver_searcher/tar.gz/#{node.the_silver_searcher.version}"
 default.the_silver_searcher.build_opt = ''
