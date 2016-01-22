d9d69198378252fa33798ea03249dd87f9c5e985
Mark Cornick <mark@markcornick.com>
2016-01-21 20:00:24 -0500

    version 1.4.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mark@markcornick.com'
 license 'MIT'
 description 'Installs/Configures The Silver Searcher'
 depends 'build-essential'
-version '1.4.2'
+version '1.4.3'
 
 %w(debian ubuntu redhat centos fedora).each do |platform|
   supports platform
